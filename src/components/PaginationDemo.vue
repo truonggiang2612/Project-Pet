f2bd5a7bc15eea24a79afa215a2ce39e03ef6f28
GiangTT <giangtt@adgad-net.vn>
2022-11-22 16:45:00 +0700

    Done 80%

--- src/components/PaginationDemo.vue
+++ src/components/PaginationDemo.vue
@@ -1,6 +1,7 @@
 <script setup>
 import axios from 'axios'
 import { ref, computed, onMounted } from 'vue';
+
 var url = "https://jsonplaceholder.typicode.com/posts"
 
 const list = ref([]);
@@ -45,7 +46,7 @@ const filteredList = computed(() => {
 <template>
     <div class="flex flex-col justify-center">
       <div class="mb-[10px]" v-for="(user, index) in filteredList" :key="index">
-        <div class="text-wrapper">
+        <div class="w-full">
           <h3>
             <strong>id:</strong> {{ user.id }}
           </h3>
@@ -55,9 +56,14 @@ const filteredList = computed(() => {
         </div>
       </div>
 
-      <div class="w-[50%] flex justify-center items-center mt-[20px]">
-        <button class="border-2 p-[8px] rounded-[50%] mr-[50px] hover:bg-[#ffcdd2] active:bg-[#f44336] active:border-[#000]" :disabled="currentPage === 1" @click="changePage(-1)">Prev</button>
-        <button class="border-2 p-[8px] rounded-[50%] mr-[50px] hover:bg-[#ffcdd2] active:bg-[#f44336] active:border-[#000]" :disabled="currentPage === 10" @click="changePage(1)">Next</button>
+      <div class="w-50% flex justify-center items-center mt-[20px]">
+        <button class="border-2 p-[8px] rounded-[50%] mr-[30px] hover:bg-[#ffcdd2] active:bg-[#f44336] active:border-[#000]" :disabled="currentPage === 1" @click="changePage(-1)">Prev</button>
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
+          </svg>
+        </div>
+        <button class="border-2 p-[8px] rounded-[50%] ml-[30px] hover:bg-[#ffcdd2] active:bg-[#f44336] active:border-[#000]" :disabled="currentPage === 10" @click="changePage(1)">Next</button>
       </div>
     </div>
 </template>
