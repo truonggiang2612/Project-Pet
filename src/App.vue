ffde40ca3705adefbe4eac820c274a9d61c2830a
GiangTT <giangtt@adgad-net.vn>
2022-11-23 14:21:06 +0700

    responsive - done

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,20 @@ import PaginationDemo from './components/PaginationDemo.vue';
 </script>
 
 <template>
-  <div class="container-app flex flex-col justify-center items-center bg-[#fff]">
-    <div class="sm:text-[20px] md:text-[30px] p-[10px] rounded mt-[20px]">
-      <div>
+  <div class="flex flex-col justify-center items-center bg-[#fff]">
+    <div class="md:text-[30px] p-[10px] rounded">
+      <div class="md:absolute md:left-[10px]">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
         class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round"
           d="M9.348 14.651a3.75 3.75 0 010-5.303m5.304 0a3.75 3.75 0 010 5.303m-7.425 2.122a6.75 6.75 0 010-9.546m9.546 0a6.75 6.75 0 010 9.546M5.106 18.894c-3.808-3.808-3.808-9.98 0-13.789m13.788 0c3.808 3.808 3.808 9.981 0 13.79M12 12h.008v.007H12V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
       </svg>
       </div>
-      <h1>Pagination - Demo</h1>
+      <p class="md:absolute md:left-[40px]">Pagination - Demo</p>
+      <span class="absolute w-[100px] h-[1px] bg-[#000] left-[145px] top-[60px] md:absolute md:w-[250px] md:left-[40px]"></span>
     </div>
 
-    <div class="p-[20px] sm:w-[80%] md:w-[50%] sm:mt-[20px] md:mt-[50px] mt-[20px]">
+    <div class="p-[20px] lg:w-[40%]">
       <PaginationDemo />
     </div>
   </div>
--- src/components/PaginationDemo.vue
+++ src/components/PaginationDemo.vue
@@ -4,17 +4,18 @@ import { ref, computed, onMounted } from 'vue';
 
 var url = "https://jsonplaceholder.typicode.com/posts"
 
-const list = ref([]);
+const lists = ref([]);
 const totalItemShow = ref(10);     // Show item = 10
 const currentPage = ref(1)    // Default page 1
-
+const textValue = ref('')
+const isShow = ref(false)
 
 // Call API get data
 const getUsers = () => {
   axios.get(url)
     .then(res => {
       console.log('res------>', res.data);
-      list.value = res.data
+      lists.value = res.data
     })
     .catch(err => {
       console.log(err);
@@ -25,12 +26,43 @@ onMounted(() => {
   getUsers()
 })
 
+// Post data
+const postData = () => {
+ if (textValue.value && textValue.value.trim()) {
+  axios.post(url, { title: textValue.value})
+      .then (res => {
+        lists.value.unshift(res.data)
+        textValue.value = ''
+        isShow.value = true
+        setTimeout(() => {
+          isShow.value = false
+        }, 2000)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+ } else {
+  alert ('Please enter value...')
+  textValue.value = ''
+ }
+}
+
+//Delete post
+const deletePost = (index) => {
+  let urlDelete = "https://jsonplaceholder.typicode.com/posts/${index}"
+  axios.delete(urlDelete)
+      .then(res => {
+        console.log(res);
+        lists.value.splice(index, 1)
+      })
+      .catch(err => {
+        console.log('Co loi =>',err);
+      })
+}
 
 // Change page
 const changePage = (num) => {
-  console.log('Số truyền vào là:', num);
-  console.log('Trang hiện tại là:', currentPage.value);
-  currentPage.value = currentPage.value + num   // Trang hiện tại + number
+  currentPage.value = currentPage.value + num
 }
 
 
@@ -38,32 +70,45 @@ const changePage = (num) => {
 const filteredList = computed(() => {
   const star = (currentPage.value - 1) * totalItemShow.value
   const end = currentPage.value * totalItemShow.value
-  const result = list.value.slice(star, end)
+  const result = lists.value.slice(star, end)
   return result
 })
 </script>
 
 <template>
     <div class="flex flex-col justify-center">
+      <div class="flex justify-start items-center mb-[20px] sm:mt-[100px]">
+          <input v-model="textValue" type="text" class="border-2 h-[30px] rounded mr-[20px] w-[200px] shadow-[0_5px_10px_5px_rgba(0,0,0,0.3)] md:w-[300px] md:h-[40px]">
+          <button @click="postData" class="border-2 p-[2px] rounded active:bg-[#0000ff] shadow-[0_5px_10px_5px_rgba(0,0,0,0.3)] md:p-[6px]">Add</button>
+          <p v-if="isShow" class="absolute w-[110px] border-2 p-[5px] rounded-[10px] bg-[#FFFFF] shadow-[0_5px_10px_10px_rgba(0,255,255,0.3)] right-[5px] ml-[200px] mb-[70px] text-[10px] text-blue-600
+          md:w-[200px] md:h-[50px] md:text-center md:leading-[35px] md:text-[16px] md:mt-[70px] md:mr-[60px]
+          lg:mr-[150px]
+          xl:mr-[250px]
+          2xl:mr-[500px]
+          ">Added successfully!</p>
+      </div>
+
       <div class="mb-[10px]" v-for="(user, index) in filteredList" :key="index">
         <div class="w-full">
-          <h3>
+          <h3 class="text-[14px] md:text-[16px]">
             <strong>id:</strong> {{ user.id }}
           </h3>
-          <p>
+          <p class="text-[14px] md:text-[16px]">
             <strong>title:</strong> {{ user.title }}
           </p>
+          <button @click="deletePost(index)" class="border-2 rounded p-[2px] text-[14px] active:bg-[#ff0000] shadow-[0_5px_10px_5px_rgba(0,0,0,0.3)] md:p-[2px]">Delete</button>
         </div>
       </div>
 
       <div class="w-50% flex justify-center items-center mt-[20px]">
-        <button class="border-2 p-[8px] rounded-[50%] mr-[30px] hover:bg-[#ffcdd2] active:bg-[#f44336] active:border-[#000]" :disabled="currentPage === 1" @click="changePage(-1)">Prev</button>
+        <button class="border-2 p-[4px] rounded-[50%] mr-[30px] hover:bg-[#ffcdd2] active:bg-[#f44336] active:border-[#000] md:p-[6px]" :disabled="currentPage === 1" @click="changePage(-1)">Prev</button>
         <div>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
           </svg>
         </div>
-        <button class="border-2 p-[8px] rounded-[50%] ml-[30px] hover:bg-[#ffcdd2] active:bg-[#f44336] active:border-[#000]" :disabled="currentPage === 10" @click="changePage(1)">Next</button>
+        <button class="border-2 p-[4px] rounded-[50%] ml-[30px] hover:bg-[#ffcdd2] active:bg-[#f44336] active:border-[#000] md:p-[6px]" :disabled="currentPage === 10" @click="changePage(1)">Next</button>
       </div>
     </div>
 </template>
+
