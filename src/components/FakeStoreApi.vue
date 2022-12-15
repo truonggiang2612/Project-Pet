52e1abf29e351788925d18dfebac5d6831cf4a23
truonggiang2612 <toidaumuonkhocprolaanhne@gmail.com>
2022-12-15 22:22:55 +0700

    new commit

--- src/components/FakeStoreApi.vue
+++ src/components/FakeStoreApi.vue
@@ -8,27 +8,27 @@ const errors = ref([])
 const getProducts = () => {
     let url = 'https://fakestoreapi.com/products'
     axios.get(url)
-            .then(res => {
-                console.log('res -------->', res.data);
-                products.value = res.data
-            })
-            .catch(err => {
-                console.log('err', err);
-                errors.value.push(err)
-            })
+        .then(res => {
+            console.log('res -------->', res.data);
+            products.value = res.data
+        })
+        .catch(err => {
+            console.log('err', err);
+            errors.value.push(err)
+        })
 }
 onMounted(() => getProducts())
 </script>
 
 <template>
     <div class="container-store">
-        <ul v-for="(item, index) in products" :key="index">
-            <li>{{ item.title }}</li>
-            <li>{{ item.price }}</li>
-            <li>
-                <img :src="item.image" alt="image" width="200" height="200">
+        <ul class="product-item" v-for="(item, index) in products" :key="index">
+            <li class="item-img">
+                <img :src="item.image" alt="image" s>
             </li>
-            <li>{{ item.rating }}</li>
+            <li class="item-title"><strong>{{ item.title }}</strong></li>
+            <li><strong>Price:</strong> {{ item.price }}</li>
+            <li><strong>Rate:</strong> {{ item.rating.rate }}</li>
         </ul>
     </div>
 </template>
@@ -37,7 +37,38 @@ onMounted(() => getProducts())
 .container-store {
     width: 100%;
     height: 100%;
-    border: 1px solid #ccc;
     display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    margin-top: 100px;
+}
+
+.product-item {
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    width: 300px;
+    height: 300px;
+    margin-right: 20px;
+    margin-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 5px 5px 5px #ccc;
+}
+
+.item-title {
+    margin: 10px
+}
+
+.item-img {
+    margin-top: 10px;
+    width: 150px;
+    height: 120px;
+}
+
+.item-img img {
+    max-width: 150px;
+    max-height: 120px;
 }
 </style>
---
+++ src/components/SortProduct.vue
@@ -0,0 +1,18 @@
+<script setup>
+import axios from 'axios';
+
+const sortProduct = () => {
+  let url = 'https://fakestoreapi.com/products?sort=desc'
+  axios.get(url)
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(err => err)
+}
+</script>
+
+<template>
+  <div class="container-sort">
+    <button @click="sortProduct()">Sort</button>
+  </div>
+</template>
